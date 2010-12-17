91988fbfaf0fa915bd358dcd4b970134ba79edf5
wyz <justin.w.xd@gmail.com>
2010-12-17 11:42:44 +0800

    no dhcp

--- src/mentohust.c
+++ src/mentohust.c
@@ -57,6 +57,7 @@ int main(int argc, char **argv)
 	signal(SIGQUIT, sig_handle);	/* Ctrl+\ */
 	signal(SIGTSTP, sig_handle);	/* Ctrl+Z */
 	signal(SIGTERM, sig_handle);	/* 被结束时 */
+    dhcpMode = 6;
     if (dhcpMode == 3)	  /* 认证前DHCP */
 		switchState(ID_DHCP);
 	else
